9d9f6cac620fbd673b64d4cc7507c9d48cf252cc
harshvaghanii <harshvaghani98@gmail.com>
2025-01-12 03:16:20 -0500

    adding communication between containers

---
+++ docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Remove the image if exists to rebuild it
+docker rmi starwars-api || { echo "No Image with the name starwars-api exists, creating a new one..."; }
+
+# Build a Docker image
+docker build -t starwars-api .
+
+# Deleting the container if exists
+docker stop starwars || { echo "No container with the name starwars exists, creating a new one..."; }
+
+# Run a Docker container
+docker run -d -p 3000:3000 --name starwars --rm starwars-api
